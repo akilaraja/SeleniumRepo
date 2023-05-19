60f79a3cf43828d27a257c4e6fda1ffb1fd998d2
akilaraja <133877654+akilaraja@users.noreply.github.com>
2023-05-19 21:31:17 +0530

    Add files via upload

---
+++ day2/FibonacciSeries.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+	
+	int n1=0, n2=1, range=7, sum=0;
+	
+	System.out.println(n1);
+	
+	for(int i=0; i<=7; i++)
+	{
+	sum=n1+n2;
+	n1=n2;
+	n2=sum;
+
+	System.out.println(sum);
+	
+	
+	}
+
+}
+}
